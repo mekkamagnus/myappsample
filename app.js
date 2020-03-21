1cd9ea2113fd2beef29da3ac1521b9b2acb23810
Mekael Turner <mistnpeace@gmail.com>
2020-03-21 15:24:49 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let header = document.getElementById("header");
+let textBox = document.getElementById("textBox");
+let button = document
+	.getElementById("button")
+	.addEventListener("click", runIt);
+let display = document.getElementById("display");
+function runIt(e) {
+	console.log(textBox.value);
+	display.innerHTML += textBox.value + "<br>";
+}
+console.log(textBox.value);
